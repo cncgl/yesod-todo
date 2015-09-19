cd7f864de44b3eba6cccb01d1ab615aed22c8dfe
Shigeru Kondou <concigel@gmail.com>
2015-09-19 22:40:00 +0900

    :four_leaf_clover: Added REST routing

--- Handler/Todo.hs
+++ Handler/Todo.hs
@@ -2,6 +2,19 @@ module Handler.Todo where
 
 import Import
 
-getTodoR :: Handler Html
+getTodosR :: Handler Value
 -- getTodoR = error "Not yet implemented: getTodoR"
-getTodoR = defaultLayout $(widgetFile "todo")
+-- getTodosR = defaultLayout $(widgetFile "todo")
+getTodosR = error "index"
+
+postTodosR :: Handler ()
+postTodosR = error "create"
+
+getTodoR :: Int -> Handler Value
+getTodoR = error "show"
+
+putTodoR :: Int -> Handler Value
+putTodoR = error "update"
+
+deleteTodoR :: Int -> Handler Value
+deleteTodoR = error "delete"
